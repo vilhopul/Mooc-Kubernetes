2ce635630a1b583fe651a088e334ce04c8e4f7c4
vilhopul <vilhopul@uef.fi>
2025-11-27 12:03:00 +0200

    Exercise 1.5

--- web_server/index.js
+++ web_server/index.js
@@ -1,11 +1,25 @@
 import http from 'http';
 
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    });
+    if (req.method === 'GET' && req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`
+            <!DOCTYPE html>
+                <html>
+                    <body>
+                        <h1>this is running inside kube!</h1>
+                    </body>
+                </html>
+        `);
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+});
 
 server.listen(PORT, () => {
     console.log(`Server started in port ${PORT}`);
